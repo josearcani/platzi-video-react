04e75909ba69e1859c7b51f6973436dbbcf025e5
josearc <juanjose.arcani2309@gmail.com>
2021-01-20 17:57:18 -0400

    Register con hooks

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,23 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import '../assets/styles/components/Register.scss'
 
-const Register = () => (
-  <section className="register">
-    <section className="register__container">
-      <h2>Regístrate</h2>
-      <form className="register__container--form">
-        <input className="input" type="text" placeholder="Nombre" />
-        <input className="input" type="text" placeholder="Correo" />
-        <input className="input" type="password" placeholder="Contraseña" />
-        <button className="button">Registrarme</button>
-      </form>
-      <Link to="/login">
-        Iniciar sesión
-      </Link>
+const Register = () => {
+
+  const [form, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  const handleInput = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setValues({
+      ...form,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(form)
+  }
+
+  return(
+    <section className="register">
+      <section className="register__container">
+        <h2>Regístrate</h2>
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input
+            name="name"
+            className="input"
+            type="text"
+            placeholder="Nombre"
+            onChange={handleInput}
+            value={form.name}
+          />
+          <input
+            name="email"
+            className="input"
+            type="email"
+            placeholder="Correo"
+            onChange={handleInput}
+            value={form.email}
+          />
+          <input
+            name="password"
+            className="input"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+            value={form.password}
+          />
+          <button
+            
+            className="button"
+          >
+            Registrarme
+          </button>
+        </form>
+        <Link to="/login">
+          Iniciar sesión
+        </Link>
+      </section>
     </section>
-  </section>
-)
+  )
+
+}
 
 export default Register
