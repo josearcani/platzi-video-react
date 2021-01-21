ba6c6c2ac503314c97895c847d41971c1c6d8e23
josearc <juanjose.arcani2309@gmail.com>
2021-01-21 16:00:12 -0400

    por terminar search y proptypes

--- src/index.js
+++ src/index.js
@@ -172,7 +172,7 @@ const initialState = {
   ]
 }
 
-const store = createStore(reducer, initialState)
+const store = createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
