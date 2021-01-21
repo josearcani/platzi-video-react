42697487fd570a84173d6f5e8a022015efa548db
josearc <juanjose.arcani2309@gmail.com>
2021-01-21 10:49:28 -0400

    Player

---
+++ src/containers/Player.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// import { Link } from 'react-router-dom'
+
+import '../assets/styles/components/Player.scss'
+
+const Player = () => {
+  return (
+    <div className="Player">
+      <video controls autoPlay>
+        <source src="" type="video/mp4" />
+      </video>
+      <div className="Player-back">
+        {/* <Link to="/"> */}
+          <button type="button">
+            Regresar
+          </button>
+        {/* </Link> */}
+      </div>
+    </div>
+  )
+}
+
+export default Player
