a7843d8bb4ba261b5d399311039e0a8f7e984385
josearc <juanjose.arcani2309@gmail.com>
2021-07-06 20:12:48 -0400

    Class 9 - Abstayendo React router, history para tener la lógica de enrutamiento separada

---
+++ src/frontend/routes/serverRoutes.js
@@ -0,0 +1,35 @@
+import Login from '../containers/Login'
+import Home from '../containers/Home'
+import Register from '../containers/Register'
+import NotFound from '../containers/NotFound'
+import Player from '../containers/Player'
+// importamos todos para el router
+
+const routes = [
+  {
+    exact: true,
+    path: '/',
+    component: Home,
+  },
+  {
+    exact: true,
+    path: '/login',
+    component: Login,
+  },
+  {
+    exact: true,
+    path: '/register',
+    component: Register,
+  },
+  {
+    exact: true,
+    path: '/player/:id',
+    component: Player,
+  },
+  {
+    name: 'Not Found',
+    component: NotFound,
+  },
+];
+
+export default routes;
