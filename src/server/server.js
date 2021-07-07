e1489c3735b1a611f7eee77b5beae9be828313ad
josearc <juanjose.arcani2309@gmail.com>
2021-07-06 22:18:34 -0400

    Class 10 - Definición de funciones para renderizado desde el servidor - nueva lib para ignorar estilos y pngs

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import webpack from 'webpack';
 
+import React from 'react'
+import { renderToString } from 'react-dom/server' // una funcionalidad especial para servir string
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { StaticRouter } from 'react-router-dom'; // importamos o instalamos como depProducción
+import { renderRoutes } from 'react-router-config' // recibe un array de rutas para renderizar la app
+
 import config from  './config';
+import reducer from '../frontend/reducer'
+import initialState from '../frontend/initialState'
+import serverRoutes from '../frontend/routes/serverRoutes'
 
 const { ENV, PORT } = config;
 
@@ -19,24 +29,39 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler)); // hacer el hot module replacement de todo el proyecto
 }
 
-app.get('*', (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="https://i.postimg.cc/T16gVyD6/badge.png" type="image/x-icon">
-    <title>Platzi Video con React</title>
-    <link rel="stylesheet" href="assets/app.css" type="text/css">
-  </head>
-  <body>
-    <div id="app"></div>
-    <script src="assets/app.js" type="text/javascript"></script>
-  </body>
-  </html>
-  `);
-});
+const setResponse = (html) => {
+  return (`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="shortcut icon" href="https://i.postimg.cc/T16gVyD6/badge.png" type="image/x-icon">
+      <title>Platzi Video con React</title>
+      <link rel="stylesheet" href="assets/app.css" type="text/css">
+    </head>
+    <body>
+      <div id="app">${html}</div>
+      <script src="assets/app.js" type="text/javascript"></script>
+    </body>
+    </html>
+  `)
+}
+
+const renderApp = (req, res) => {
+  // definimos el store pero sin usar los dev tools de redux
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  );
+  res.send(setResponse(html));
+};
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
